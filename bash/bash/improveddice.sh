01d72377b2fd9220a941a06331735b79bb5e4304
Your Name <your@yourdomain>
2019-06-10 18:24:36 -0500

    final

--- bash/bash/improveddice.sh
+++ bash/bash/improveddice.sh
@@ -2,6 +2,7 @@
 #
 # this script rolls a pair of six-sided dice and displays both the rolls and the sum
 #
+
 # Improve this script by re-organizing it to:
 #  put the number of sides in a variable which is used as the range for the random number
 #  put the bias, or offset, for the RANDOM number in a variable (it is 1 for our purposes)
@@ -10,6 +11,7 @@
 #  generate the sum of the dice
 #  generate the average of the dice
 #  display a summary of what was rolled, and what the result was
+
 # Tell the user we have started processing
 echo "Rolling..."
 sides=6
